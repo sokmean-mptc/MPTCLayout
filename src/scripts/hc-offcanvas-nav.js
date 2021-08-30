6660fa9843576c77a11ed0a0f3c869a8becdfd57
sokmean-mptc <ngonsokmean@mptc.gov.kh>
2021-08-30 11:28:14 +0700

    #

--- src/scripts/hc-offcanvas-nav.js
+++ src/scripts/hc-offcanvas-nav.js
@@ -1,11 +1,12 @@
 import hcOffcanvasNav from 'hc-offcanvas-nav'
-if ( document.getElementById("main-nav") )
-document.addEventListener('DOMContentLoaded', function() {
-    new hcOffcanvasNav('#main-nav', {
-        disableAt: 992,
-        customToggle: '.toggle-nav',
-        insertClose: false,
-        insertBack: false,
-        position: 'right'
+if ( document.getElementById("main-nav") ) {
+    document.addEventListener('DOMContentLoaded', function() {
+        new hcOffcanvasNav('#main-nav', {
+            disableAt: 992,
+            customToggle: '.toggle-nav',
+            insertClose: false,
+            insertBack: false,
+            position: 'right'
+        })
     })
-})
+}
